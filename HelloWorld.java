f413a1994ea069e37cfc88e0df293609b91989b8
Olivia Gette <omgette@mtu.edu>
2025-02-20 10:07:54 -0500

    Delete HelloWorld.java file

--- HelloWorld.java
+++
@@ -1,6 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] arg){
-        System.out.println("Hello World.");
-        System.out.println("This is Olivia and Ewa in a Michigan Tech’s CS1121 Lab taught by Sarah, writing our first ever programs!");
-    }
-}
